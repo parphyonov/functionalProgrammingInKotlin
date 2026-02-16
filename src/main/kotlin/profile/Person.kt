2f920561af67aa2d230081f4d1812ab638cc58d4
Alexei Parphyonov <alexeiparphyonov@gmail.com>
2026-02-16 20:11:53 +0300

    first commit

---
+++ src/main/kotlin/profile/Person.kt
@@ -0,0 +1,12 @@
+package profile
+
+data class Person(
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val age: Int,
+    val gender: String,
+    val email: String,
+    val address: String,
+    val phoneNumber: Long
+)
