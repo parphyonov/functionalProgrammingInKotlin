cc01a47675b32757002888aed6552e9b01494298
Alexei Parphyonov <alexeiparphyonov@gmail.com>
2026-02-16 22:17:21 +0300

    added some more files, studied pure functions

--- src/main/kotlin/test/Test.kt
+++ src/main/kotlin/test/Test.kt
@@ -1,27 +1,21 @@
 package test
 
-import kotlinx.serialization.json.Json
 import java.io.File
 
+//val numbers = mutableListOf<Int>()
+
 fun main() {
-    val bookShelf = listOf(
-        Book("Ivan Pavlov in Leningrad", "Various", 1986),
-        Book("The Word is God", "Lyutsiya Drozdova-Shichko", 1995),
-        Book("The Left Hand of Darkness", "Urlula K. LeGuin", 1964),
-        Book("Fiasco", "Stanislaw Lem", 1971),
-        Book("A Century Too Little for Men", "Fyodor Uglov", 1988),
-        Book("Memorandum to Humanity", "Aleksandr Shichko", 1993),
-        Book("A Beetle on the Anthill", "Arkady and Boris Strugatzkys", 1964),
-        Book("Time Wanderers", "Arkady and Boris Strugatzkys", 1966),
-        Book("One Billion Years Before the End of the World", "Arkady and Boris Strugazkys", 1971),
-        Book("Fuckup", "Konstantin Krylov", 2009)
-    )
+    val file = File("test.txt")
+    val b = file.readText().trim().toInt()
 
-    val bookFile = File("books.json")
-    bookFile.writeText(Json.encodeToString(bookShelf))
-
-    val reopenedShelf = Json.decodeFromString<List<Book>>(bookFile.readText())
-
-    for (book in reopenedShelf)
-        println(book)
+    println(sum(5, b))
 }
+
+//fun sum(a: Int, b: Int): Int {
+//    val res = a + b
+//
+//    numbers.add(res)
+//    return res
+//}
+
+fun sum(a: Int, b: Int): Int = a + b
