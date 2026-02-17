4c2cb33e77c306b7a303687b28c443091ebde5c8
Alexei Parphyonov <alexeiparphyonov@gmail.com>
2026-02-17 17:05:49 +0300

    5.6 Generics intermediate commit

--- src/main/kotlin/profile/Profile.kt
+++ src/main/kotlin/profile/Profile.kt
@@ -8,12 +8,21 @@ fun main() {
     filtered = filter(filtered) { it.firstName.startsWith("A") }
     filtered = filter(filtered) { it.gender == Gender.MALE }
 
-    for (person in filtered) {
-        println(person)
-    }
+    val fullNames = transform(filtered) { "${it.firstName} ${it.lastName}"}
 
-    println("Total ${profiles.size}")
-    println("Filtered: ${filtered.size}")
+    val selectedCustomers =
+        transform(filtered) { it.copy(age = it.age + 1) }
+
+    for (customer in selectedCustomers)
+        println("$customer")
+}
+
+fun <R> transform(profiles: List<Person>, transformation: (Person) -> R): List<R> {
+    val result = mutableListOf<R>()
+    for (person in profiles) {
+        result.add(transformation(person))
+    }
+    return result
 }
 
 fun filter(profiles: List<Person>, isSuitable: (Person) -> Boolean): List<Person> {
